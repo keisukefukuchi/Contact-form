12a0a4afdc839fa44bce9f2f6ab73caf6d76eda6
keisukefukuchi <kekke53530708@gmail.com>
2021-12-25 15:47:56 +0900

    message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email',191)->unique();
-            $table->timestamp('email_verified_at',191)->nullable();
-            $table->string('password',191);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
