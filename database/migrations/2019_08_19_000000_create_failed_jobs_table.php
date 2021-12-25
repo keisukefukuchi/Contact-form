e2385bb509d6acc843ffdd968c3b0bf5aa1c28e4
keisukefukuchi <kekke53530708@gmail.com>
2021-12-25 15:37:13 +0900

    message

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -15,11 +15,11 @@ class CreateFailedJobsTable extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
+            $table->string('uuid',191)->unique();
+            $table->text('connection',191);
+            $table->text('queue',191);
+            $table->longText('payload',191);
+            $table->longText('exception',191);
             $table->timestamp('failed_at')->useCurrent();
         });
     }
