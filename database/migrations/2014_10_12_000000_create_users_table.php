e2b0d37aeef3757d975872bb911c750352471486
keisukefukuchi <kekke53530708@gmail.com>
2021-12-26 10:12:06 +0900

    message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at', 191)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
