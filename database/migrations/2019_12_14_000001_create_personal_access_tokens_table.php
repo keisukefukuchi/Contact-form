be221577986b1fa96aab8305c8dc72db0e05572a
keisukefukuchi <kekke53530708@gmail.com>
2021-12-26 10:21:23 +0900

    message

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,7 +14,7 @@ class CreatePersonalAccessTokensTable extends Migration
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id' ,191);
             $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 191)->unique();
